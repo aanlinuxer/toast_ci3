0424b85ef12271fb8609b625b4dee6e45362d15b
Aan Kurniawan <aan.linuxer@gmail.com>
2016-05-16 21:24:22 +0700

    Add ENVIRONMENT check
    Check if ENVIRONMENT !== 'testing'

--- application/controlles/test/Example_test.php
+++ application/controlles/test/Example_test.php
@@ -3,59 +3,59 @@ require_once(APPPATH . '/controllers/test/Toast.php');
 
 class Example_tests extends Toast
 {
-	function __construct()
-	{
-		parent::__construct(__FILE__);
-		// Load any models, libraries etc. you need here
-	}
+    function __construct()
+    {
+        parent::__construct(__FILE__);
+        // Load any models, libraries etc. you need here
+    }
 
-	/**
-	 * OPTIONAL; Anything in this function will be run before each test
-	 * Good for doing cleanup: resetting sessions, renewing objects, etc.
-	 */
-	function _pre() {}
+    /**
+     * OPTIONAL; Anything in this function will be run before each test
+     * Good for doing cleanup: resetting sessions, renewing objects, etc.
+     */
+    function _pre() {}
 
-	/**
-	 * OPTIONAL; Anything in this function will be run after each test
-	 * I use it for setting $this->message = $this->My_model->getError();
-	 */
-	function _post() {}
-
-
-	/* TESTS BELOW */
-
-	function test_simple_addition()
-	{
-		$var = 2 + 2;
-		$this->_assert_equals($var, 4);
-	}
+    /**
+     * OPTIONAL; Anything in this function will be run after each test
+     * I use it for setting $this->message = $this->My_model->getError();
+     */
+    function _post() {}
 
 
-	function test_that_fails()
-	{
-		$a = 1;
+    /* TESTS BELOW */
 
-		// You can test multiple assertions / variables in one function:
-
-		$this->_assert_true($a); // fail
-
-		// Since one of the assertions failed, this test case will fail
-	}
+    function test_simple_addition()
+    {
+        $var = 2 + 2;
+        $this->_assert_equals($var, 4);
+    }
 
 
-	function test_or_operator()
-	{
-		$a = true;
-		$b = false;
-		$var = $a || $b;
+    function test_that_fails()
+    {
+        $a = 1;
 
-		$this->_assert_true($var);
+        // You can test multiple assertions / variables in one function:
 
-		// If you need to, you can pass a message /
-		// description to the unit test results page:
+        $this->_assert_true($a); // fail
 
-		$this->message = '$a || $b';
-	}
+        // Since one of the assertions failed, this test case will fail
+    }
+
+
+    function test_or_operator()
+    {
+        $a = true;
+        $b = false;
+        $var = $a || $b;
+
+        $this->_assert_true($var);
+
+        // If you need to, you can pass a message /
+        // description to the unit test results page:
+
+        $this->message = '$a || $b';
+    }
 
 }
 
